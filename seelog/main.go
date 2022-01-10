05c6a11b7754c231904c38b4af8147a8b34dc8fd
tanght1994 <tanght1994@outlook.com>
2022-01-11 00:14:54 +0800

    1

--- seelog/main.go
+++ seelog/main.go
@@ -1,10 +1,6 @@
 package main
 
 import (
-	"crypto/md5"
-	"encoding/hex"
-	"fmt"
-	"io/ioutil"
 	"log"
 	"time"
 
@@ -14,47 +10,28 @@ import (
 const G_LogConfigPath = "seelog.xml"
 
 func main() {
-	logger, err := seelog.LoggerFromConfigAsFile("seelog.xml")
-	if err != nil {
-		log.Fatal(err)
+	if err := initLoggerFromFile(G_LogConfigPath); err != nil {
+		log.Fatal("init log error, ", err.Error())
 	}
 
-	for i := 0; i < 1000; i++ {
-		logger.Trace("Trace")
-		logger.Debug("Debug")
-		logger.Info("Info")
-		logger.Warn("Warn")
-		logger.Error("Error")
-		logger.Flush()
-		fmt.Println("-------------------")
+	for i := 0; i < 100; i++ {
+		go work()
+	}
+
+	for {
 		time.Sleep(1 * time.Second)
 	}
 }
 
-func replaceLogger() error {
-	logger, err := seelog.LoggerFromConfigAsFile(G_LogConfigPath)
-	if err != nil {
-		return fmt.Errorf("create logger error, %s", err.Error())
+func work() {
+	for {
+		seelog.Trace("Trace")
+		seelog.Debug("Debug")
+		seelog.Info("Info")
+		seelog.Warn("Warn")
+		seelog.Error("Error")
+		seelog.Critical("-------------------")
+		seelog.Flush()
+		time.Sleep(1 * 100 * time.Millisecond)
 	}
-	return seelog.ReplaceLogger(logger)
-}
-
-type monitorLogConfig struct {
-	lastmd5 string
-	path    string
-}
-
-func (m *monitorLogConfig) monitor() {
-	bys, err := ioutil.ReadFile(m.path)
-	if err != nil {
-		// log
-		return
-	}
-	t1 := md5.Sum(bys)
-	t2 := hex.EncodeToString(t1[:])
-	if t2 == m.lastmd5 {
-		return
-	}
-	m.lastmd5 = t2
-	replaceLogger()
 }
