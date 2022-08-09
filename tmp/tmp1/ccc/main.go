2d48bdc2026a4b26ac3cbcd4dacd85c6137bc9fa
tanght <tanght@mail.jj.cn>
2022-08-09 14:03:50 +0800

    1

--- tmp/tmp1/ccc/main.go
+++ tmp/tmp1/ccc/main.go
@@ -3,19 +3,22 @@ package main
 import (
 	"fmt"
 	"os"
-	"time"
 )
 
 func main() {
-	fs, err := os.Open("C:/Users/tanght/Desktop/tmp/gostudy/tmp/tmp1/tmp.txt")
-	must(err)
-	bys := make([]byte, 10)
-	for i := 0; i < 1000000; i++ {
-		n, err := fs.Read(bys)
-		must(err)
-		fmt.Println(n, " | ", string(bys))
-		time.Sleep(100 * time.Millisecond)
-	}
+	a := map[string]int{}
+	hahahahahahaha(a)
+	fmt.Println(a)
+}
+
+func hahahahahahaha(data map[string]int) {
+	data["a"] = 1
+	data["b"] = 1
+	data["c"] = 1
+	data["d"] = 1
+	data["e"] = 1
+	data["f"] = 1
+	data["g"] = 1
 }
 
 func must(err error) {
--- tmp/tmp1/main.go
+++ tmp/tmp1/main.go
@@ -1,29 +1,86 @@
 package main
 
 import (
+	"crypto/md5"
 	"fmt"
-	"io/ioutil"
-	"net/http"
+	"time"
 
 	_ "github.com/go-sql-driver/mysql"
+	"github.com/jmoiron/sqlx"
 )
-
-func main() {
-	server := Server{}
-	http.ListenAndServe("127.0.0.1:8000", server)
-}
-
-type Server struct{}
-
-func (s Server) ServeHTTP(res http.ResponseWriter, req *http.Request) {
-	bys, err := ioutil.ReadAll(req.Body)
-	must(err)
-	fmt.Println(string(bys))
-	res.Write([]byte("Hello World"))
-}
 
 func must(err error) {
 	if err != nil {
 		panic(err)
 	}
+}
+
+type LogData struct {
+	Id      int
+	Log     string
+	Errsign string
+}
+
+func main() {
+	dsn := "root:1234@tcp(192.168.7.45:3309)/hw_flog?interpolateParams=True&multiStatements=True"
+	db, err := sqlx.Open("mysql", dsn)
+	must(err)
+	db.SetMaxOpenConns(1)
+	db.SetConnMaxIdleTime(100 * time.Minute)
+	db.SetConnMaxLifetime(100 * time.Hour)
+
+	errsign := make(map[[16]byte]int)
+	log := make(map[[16]byte]int)
+
+	id := 634782
+	data := make([]LogData, 0)
+	for {
+		data = data[0:0]
+		db.Select(&data, "SELECT id, errsign, log FROM client_log WHERE id>? LIMIT 10000", id)
+		if len(data) == 0 {
+			break
+		}
+		id = data[len(data)-1].Id
+		fmt.Println(id)
+		for _, d := range data {
+			errsign[md5.Sum([]byte(d.Errsign))]++
+			log[md5.Sum([]byte(d.Log))]++
+		}
+	}
+	fmt.Println("-------")
+	fmt.Println(len(errsign))
+	fmt.Println(len(log))
+}
+
+
+func getid(db *sqlx.DB, date string) int {
+	id := 0
+	err := db.Get(&id, "SELECT id FROM client_log WHERE gmt_create >= ? LIMIT 1", date)
+	must(err)
+	return id
+}
+
+type aggregationData struct {
+	id int
+	data string
+}
+
+func aggregation(db *sqlx.DB, field string, start int) {
+	data := make([]LogData, 0)
+	for {
+		data = data[0:0]
+		db.Select(&data, "SELECT id, errsign, log FROM client_log WHERE id>? LIMIT 10000", id)
+		if len(data) == 0 {
+			break
+		}
+		id = data[len(data)-1].Id
+		fmt.Println(id)
+		for _, d := range data {
+			errsign[md5.Sum([]byte(d.Errsign))]++
+			log[md5.Sum([]byte(d.Log))]++
+		}
+	}
+	fmt.Println("-------")
+	fmt.Println(len(errsign))
+	fmt.Println(len(log))
 }
