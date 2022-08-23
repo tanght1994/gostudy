5c3eb0d7db19f2582ac9b908b50bb2d98e916d0c
tanght <tanght@mail.jj.cn>
2022-08-23 21:04:56 +0800

    1

--- tmp/tmp1/main.go
+++ tmp/tmp1/main.go
@@ -1,11 +1,7 @@
 package main
 
 import (
-	"bufio"
-	"bytes"
-	"crypto/sha1"
 	"fmt"
-	"io"
 
 	_ "github.com/go-sql-driver/mysql"
 )
@@ -17,6 +13,38 @@ func must(err error) {
 }
 
 func main() {
-	bufio.NewReader()
-	bytes.NewReader()
+
+	fmt.Println(maxSlidingWindow([]int{7, 2, 4}, 2))
+}
+
+func maxSlidingWindow(nums []int, k int) []int {
+	res := []int{}
+	que := []int{}
+	que = append(que, nums[0])
+	for i := 1; i < k; i++ {
+		for len(que) != 0 {
+			if que[len(que)-1] < nums[i] {
+				que = que[:len(que)-1]
+			} else {
+				break
+			}
+		}
+		que = append(que, nums[i])
+	}
+	res = append(res, que[0])
+	for i := k; i < len(nums); i++ {
+		if que[len(que)-1] == nums[i-k] {
+			que = que[1:]
+		}
+		for len(que) != 0 {
+			if que[len(que)-1] < nums[i] {
+				que = que[:len(que)-1]
+			} else {
+				break
+			}
+		}
+		que = append(que, nums[i])
+		res = append(res, que[0])
+	}
+	return res
 }
